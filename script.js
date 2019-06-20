ebfe93475eebfb0a506419fcc5d0386a071049d9
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-06-20 15:53:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let speed = 90;
+
+if(speed > 60){
+console.log("you are  overspeeding");
+}else {
+    console.log
+("No overspeeding");
+}
+*/
+
+// let isLogedin= true;
+// if(isLogedin){
+// alert("wlocom Maham ");
+// }else {
+//     alert
+// ("please loged in to continue!");
+// }
+
+// let temperature= 40;
+// if(temperature>40){
+// alert("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave");
+// }else {
+//     alert
+// ("No off today!");
+// }
+
+// let amount = 1000000;
+// if(amount>100000){
+//     alert("Congratulations! You are a millionaire!");
+
+// }else{
+//     alert("I'm sorry, you are not a millionaire");
+// }
